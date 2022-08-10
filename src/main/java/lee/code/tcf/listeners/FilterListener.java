c54f219141aee7fbb59140b6d49ffd8010759c5e
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-08-10 20:12:17 +0800

    更新

--- src/main/java/lee/code/tcf/listeners/FilterListener.java
+++ src/main/java/lee/code/tcf/listeners/FilterListener.java
@@ -1,5 +1,7 @@
 package lee.code.tcf.listeners;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import lee.code.tcf.TabCompleteFilter;
@@ -16,13 +18,26 @@ public class FilterListener implements Listener {
         TabCompleteFilter plugin = TabCompleteFilter.getPlugin();
 
         Player player = e.getPlayer();
-        if (!player.isOp()) {
-            if (!player.hasPermission("tcf.bypass")) {
-                e.getCommands().clear();
-                for (String group : plugin.getData().getGroups()) {
-                    if (player.hasPermission("tcf." + group)) {
-                        List<String> whitelist = plugin.getData().getGroupList(group);
-                        if (!whitelist.isEmpty()) for (String command : whitelist) e.getCommands().add(command.replaceFirst("/", ""));
+        if (!player.isOp() && !player.hasPermission("mineblock.bypass.tabcompletefilter")) { // MineBlock
+            for (String group : plugin.getData().getGroups()) {
+                if (player.hasPermission("rank." + group)) {
+                    List<String> whitelist = plugin.getData().getGroupList(group);
+                    if (!whitelist.isEmpty()) {
+
+                        if (!plugin.getConfig().getBoolean("config.use-as-blacklist", true)) { // MineBlock
+                            e.getCommands().retainAll(whitelist);
+                        } else { // MineBlock
+                            Collection<String> results = new ArrayList<>();
+                            for (String cmd : e.getCommands()) { // MineBlock
+                                for (String command : whitelist) {
+                                    if (cmd.contains(command)) { // MineBlock
+                                        results.add(cmd); // MineBlock
+                                        break;
+                                    }
+                                }
+                            }
+                            e.getCommands().removeAll(results);
+                        }
                     }
                 }
             }
